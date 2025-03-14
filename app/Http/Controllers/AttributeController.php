6546f5a4eef57c51f993cf866abc82086c614602
kavi11111 <kaviyarasan1232000@gmail.com>
2025-03-14 23:06:19 +0530

    int push

--- app/Http/Controllers/AttributeController.php
+++ app/Http/Controllers/AttributeController.php
@@ -4,17 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Attribute;
+use Illuminate\Http\JsonResponse;
 
 class AttributeController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
         $attributes = Attribute::all();
         return response()->json($attributes);
     }
 
     // create new attribute
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
         $request->validate([
             'name' => 'required|string|unique:attributes',
@@ -26,13 +27,13 @@ class AttributeController extends Controller
     }
 
     // get single attribute
-    public function show(Attribute $attribute)
+    public function show(Attribute $attribute): JsonResponse
     {
         return response()->json($attribute);
     }
 
     // update attribute
-    public function update(Request $request, Attribute $attribute)
+    public function update(Request $request, Attribute $attribute): JsonResponse
     {
         $request->validate([
             'name' => 'sometimes|string|unique:attributes,name,' . $attribute->id,
@@ -44,7 +45,7 @@ class AttributeController extends Controller
     }
 
     // delete attribute
-    public function destroy(Attribute $attribute)
+    public function destroy(Attribute $attribute): JsonResponse
     {
         $attribute->delete();
         return response()->json(null, 204);
